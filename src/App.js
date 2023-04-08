e209506794f9da783c428206a81b163cd204b021
thdud4152 <thdud41522@gmail.com>
2023-04-08 11:35:53 +0900

    ver06

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import RouterPage from './components/RouterPage';
+import any from './images/any.jpg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <img src={any} width="100%"/>
+            <RouterPage/>
+        </div>
+    );
 }
 
 export default App;
